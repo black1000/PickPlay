4ca464645d6d877180fece9196fc95fe2753c09b
runa <tyrry1030@icloud.com>
2025-07-12 00:00:48 +0900

    main.js を更新

--- main.js
+++ main.js
@@ -447,10 +447,6 @@ function toggleLanguage() {
   pickCharacter();
 }
 
-if (window.navigator.standalone) {
-  alert("iPhoneのホーム画面から開くと、文字が入力できない場合があります。Safariから直接開いてください。");
-}
-
 window.onload = () => {
   loadData();
   render();
